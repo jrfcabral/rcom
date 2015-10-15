fccab765606dbdf39f7569b8331a910c8c95b00b
LuísCosta <luistelmocosta@gmail.com>
2015-10-15 22:43:16 +0100

    Application Layer added

--- linklayer.c
+++ linklayer.c
@@ -1,4 +1,5 @@
 #include "linklayer.h"
+#include "ApplicationLayer.h"
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -13,7 +14,9 @@ const int FLAG = 0x7E;
 const int A = 0x03;
 const int E = 0x7D;
 
-LinkLayer* ll;
+LinkLayer* l1;
+
+struct termios oldtio, newtio; //old and new termios
 
 int linkLayerInit(char* port, ConnectionMode mode, int baudrate, int msgMaxSize, int numTries, int timeOut){
 
@@ -30,7 +33,57 @@ int saveSettings(){
 
 int setTermios(){
 
+	bzero(&l1->newtio, sizeof(&l1->newtio)); 
+	l1->newtio.c_cflag = B38400|CS8|CLOCAL|CREAD;
+	l1->newtio.c_iflag = IGNPAR|ICRNL;
+	l1->newtio.c_oflag = 0;
+	l1->newtio.c_lflag = ICANON;
+	l1->newtio.c_cc[VTIME] = 3; /* temporizador entre   
+								caracteres*/
+	l1->newtio.c_cc[VMIN] = 0;  /* bloqueia até ler 5 
+								caracteres */
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd,TCSANOW,&l1->newtio);
+
 }
+
+int openSerialPort(char * port){
+
+	int fdport = open(port, O_RDWR | O_NOCTTY);
+
+	return fdport; //returns, if successfully, the file descriptor of the port we're trying to open. 
+}
+
+
+int closeSerialPort() {
+
+	if (tcsetattr(al->fd, TCSANOW, &l1->oldtio) == -1) { //sets the parameters (immediately) refered by the application layer fd 
+														 // from the old termios 
+		perror("tcsetattr");
+		return 0;
+	}
+
+	close(al->fd); //closes the application layer
+
+	return 1;
+}
+
+unsigned char* createCommand(ControlField C) {
+	CMD_SIZE = 5*sizeof(char);
+	unsigned char* command = malloc(CMD_SIZE);
+
+	command[0] = FLAG;
+	command[1] = A;
+	command[2] = C;
+	if (C == C_REJ || C == C_RR)
+		command[2] |= (ll->sequenceNumber << 7);
+	command[3] = command[1] ^ command[2];
+	command[4] = FLAG;
+
+	return command;
+}
+
+
 
 //openserialport
 //closeSerialPort
--- linklayer.h
+++ linklayer.h
@@ -1,3 +1,6 @@
+/*In computer networking, the link layer is the lowest layer in the Internet Protocol Suite. 
+The link layer is the group of methods and communications protocols that only operate on the link that a host is physically connected to. 
+*/
 #pragma once 
 #include <termios.h>
 #include <stdio.h>
@@ -11,17 +14,31 @@ unsigned int info;
 #define BIT(n) (0x01 << n)
 
 
+/*FRAME FORMAT AND TYPES*/
+
 typedef enum {
-	SEND, RECEIVE
-} ConnnectionMode;
+	I_START, I_FLAG, I_A, I_C, I_BCC, I_STOP
+} State; //tramas de informacao
+
+typedef enum {
+	SET, //set up
+	UA,  //unnumbered acknowledgement
+	RR,  //receiver ready 
+	REJ, //reject / negative ACK
+	DISC //disconnect
+} Control;
+
+typedef enum {
+	C_SET = 0x03, C_UA = 0x07, C_RR = 0x05, C_REJ = 0x01, C_DISC = 0x0B
+} ControlField; //campo de controlo das tramas de supervisão e nao numeradas 
 
 typedef struct{
 	char port[50] //port : format /dev/ttySx
 	int baudRate;
 	int msgMaxSize;
-	info ns; //frame sequece number
+	info sequenceNumber; //frame sequece number
 	info timeOut; //timeout value
-	info numTries //number of retries if it fails
+	info numTransmissions //number of retries if it fails
 
 	char frame[MAX_SIZE]
 
@@ -33,5 +50,5 @@ typedef struct{
 
 }LinkLayer;
 
-LinkLayer ll;
+extern LinkLayer* ll;
 
