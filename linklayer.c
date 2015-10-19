e4964a03ea6e447703af4ca6f020cb3267df716f
João Cabral <up201304395@fe.up.pt>
2015-10-19 19:51:51 +0000

    Began work on llwrite. Implemented BCC generator function and added possibility of changing the ports and the transmission mode

--- linklayer.c
+++ linklayer.c
@@ -31,8 +31,8 @@ typedef enum {
 	WAIT_A,
 	WAIT_C,
 	WAIT_BCC,
-  	BCC_OK,
-    	EXIT	
+	BCC_OK,
+	EXIT	
 } state;
 
 state verifyByte(unsigned char expected, unsigned char read, state ifSucc, state ifFail){
@@ -49,20 +49,20 @@ state verifyByte(unsigned char expected, unsigned char read, state ifSucc, state
 	return toGo;
 }
 
-
 int main(int argc, char **argv){
-	/*char *arg = (char *) malloc(strlen(argv[1])*sizeof(char)+1);
-	strcpy(arg, argv[1]);
-	char *tok1, *tok2;
-	tok1 = strtok(argv[1], "/");
-	tok2 = strtok(NULL, "/");*/
-	if(argc != 2 /*|| (!strcmp(tok1, "dev") && !strncmp(tok2, "ttyS", 5))*/){
+	strcpy(ll.port, argv[1]);
+	int mode = atoi(argv[2]);
+	if(argc != 2) {
 		printf("Usage: %s /dev/ttySx\n x = port num\n", argv[0]);
 	}
+
+	ll.timeOut = 10;
+	ll.sequenceNumber = 0;
+	ll.numTransmissions  = 3;
 	char buffer[] = {FLAG, FLAG, ESCAPE, ESCAPE, 0x6e};
 	char* stuffedBuffer;
 	int r = byteStuffing(buffer, 5, &stuffedBuffer);
-	//llopen(0, RECEIVE);
+	llopen(0, mode);
 }
 
 int byteStuffing(const char* buffer, const int length, char** stuffedBuffer){
@@ -93,32 +93,45 @@ int byteStuffing(const char* buffer, const int length, char** stuffedBuffer){
 		}
 	}
 	return newLength;
-	
+
 }
 
+char generateBCC(const char* buffer, const int length){
+	int i;
+	char bcc = 0;
+	for(i = 0; i < length; i++)
+		bcc ^=buffer[i];
+	return bcc;
+}
+
+
 int llwrite(int fd, char* buffer, int length){
-	
+	char dataBCC = generateBCC(buffer, length);
+	char *bufferStuffed;
+	char header[] = { FLAG, 0x03, (ll.sequenceNumber << 5), header[1]^header[2], FLAG};
+	int n= byteStuffing(buffer, &bufferStuffed, length);
+	int j;
+	for(j = 0; j < n; j++){
+	}
 }
 
 int llopen(int port, int mode){
 
-	
+
 	installAlarm();
 	int fd;
-	
+
 	char fileName[15];
-    sprintf(fileName, "/dev/ttyS%d", port);
-	strcpy(ll.port, fileName);
 	fd = open(ll.port, O_RDWR|O_NOCTTY);
 	if(fd <0){
 		perror(fileName);
 		exit(-1);	
 	}
-	
+
 	ll.baudRate = BAUDRATE;
 	ll.timeOut = 3;
 	ll.numTransmissions = 3;
-	
+
 	struct termios oldTio, newTio;
 
 	if(tcgetattr(fd, &oldTio) == -1){
@@ -126,134 +139,135 @@ int llopen(int port, int mode){
 		exit(-1);	
 	}
 	ll.oldtio = oldTio;
-	
+
 	memset(&newTio, 0, sizeof(newTio)); //possible error here. if error use bzero instead
 	newTio.c_cflag = ll.baudRate | CS8 | CLOCAL | CREAD;
-    	newTio.c_iflag = IGNPAR;
-    	newTio.c_oflag = 0;
+	newTio.c_iflag = IGNPAR;
+	newTio.c_oflag = 0;
 
-    	/* set input mode (non-canonical, no echo,...) */
-    	
+	/* set input mode (non-canonical, no echo,...) */
+
 
 	if(mode == RECEIVE){
 		newTio.c_lflag = 0;
 
-    		newTio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    		newTio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-		
+		newTio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+		newTio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
 		tcflush(fd, TCIOFLUSH);
-	
+
 		if ( tcsetattr(fd,TCSANOW,&newTio) == -1) {
-	      		perror("tcsetattr");
-	     		exit(-1);
-	    	}
+			perror("tcsetattr");
+			exit(-1);
+		}
 
-        printf("Ready to read\n");
-        state currentState = WAIT_FLAG;
+		printf("Ready to read\n");
+		state currentState = WAIT_FLAG;
 		printf("%d\n", currentState);
-        char buf[255];
-        int n = 0;
-        while(currentState != EXIT){
-           unsigned char in;
-           
-            int l = read(fd, &in, 1);
-            char correctBcc;
-            if (!l)
-                continue;
-            if(n >= 2)
-                 correctBcc = buf[1]^buf[2];
+		char buf[255];
+		int n = 0;
+		while(currentState != EXIT){
+			unsigned char in;
 
-            printf("read: %x\n", in);			
+			int l = read(fd, &in, 1);
+			char correctBcc;
+			if (!l)
+				continue;
+			if(n >= 2)
+				correctBcc = buf[1]^buf[2];
 
-            switch(currentState){
-                case WAIT_FLAG:
+			printf("read: %x\n", in);			
+
+			switch(currentState){
+				case WAIT_FLAG:
 					currentState = verifyByte(FLAG, in, WAIT_A, WAIT_FLAG);                  
-                break;
-                case WAIT_A:
+					break;
+				case WAIT_A:
 					currentState = verifyByte(A_SEND, in, WAIT_C, WAIT_FLAG);                 
-                break;
-                case WAIT_C:
+					break;
+				case WAIT_C:
 					currentState = verifyByte(C_SET, in, WAIT_BCC, WAIT_FLAG);                   
 					break;
-                case WAIT_BCC: 
-	               currentState = verifyByte(0x04, in, BCC_OK, WAIT_FLAG);                 
-	               break;
-                   
-                case BCC_OK:
+				case WAIT_BCC: 
+					currentState = verifyByte(0x04, in, BCC_OK, WAIT_FLAG);                 
+					break;
+
+				case BCC_OK:
 					currentState = verifyByte(FLAG, in, EXIT, WAIT_FLAG);                 
-                    break;
-                default:
-                    perror("Something very strange happened\n");
-                    exit(-3);
-                    break;                        
-                
-            }
-            
-        }
+					break;
+				default:
+					perror("Something very strange happened\n");
+					exit(-3);
+					break;                        
+
+			}
+
+		}
 		printf("Received SET frame\n");
-        unsigned char UA[5] = {FLAG, A_RECEIVE, C_UA, UA[1]^UA[2], FLAG};
-        write(fd, UA, 5);
-       
-		
+		unsigned char UA[5] = {FLAG, A_RECEIVE, C_UA, UA[1]^UA[2], FLAG};
+		write(fd, UA, 5);
+
+
 	}
 	else if(mode == SEND){
 		newTio.c_lflag = 0;
 
-    		newTio.c_cc[VTIME]    = 10;   /* inter-character timer unused */
-    		newTio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
-		
+		newTio.c_cc[VTIME]    = 10;   /* inter-character timer unused */
+		newTio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
+
 		tcflush(fd, TCIOFLUSH);
-		
+
 		if ( tcsetattr(fd,TCSANOW,&newTio) == -1) {
-      			perror("tcsetattr");
-     			exit(-1);
-        	}
+			perror("tcsetattr");
+			exit(-1);
+		}
 
-send: ;		resend = 0;
-			unsigned char SET[5] = {FLAG, A_SEND, C_SET, SET[1]^SET[2], FLAG};
-			if(	write(fd, SET, 5) != 5)
-				return -1;
-			printf("escrevi\n");	
-			alarm(ll.timeOut);
-			state currentState = WAIT_FLAG;
-			while(currentState != EXIT){
+send: ;	
+      resend = 0;
+      unsigned char SET[5] = {FLAG, A_SEND, C_SET, SET[1]^SET[2], FLAG};
+      if(	write(fd, SET, 5) != 5)
+	      return -1;
+      printf("escrevi\n");	
+      alarm(ll.timeOut);
+      state currentState = WAIT_FLAG;
+      while(currentState != EXIT){
 
-					unsigned char in;
-					if(!read(fd, &in, 1)){
-						printf("Tou aqui\n");
-						if(resend)
-							goto send;
-						else if (abort_send)
-							return -1;
-						continue;
-					}
-					
-					printf("%x\n", in);
-					printf("Current state is %d\n", currentState);
-			
-				  switch(currentState){
-		            case WAIT_FLAG:
-						currentState = verifyByte(FLAG, in, WAIT_A, WAIT_FLAG);                  
-			            break;
-		            case WAIT_A:
-						currentState = verifyByte(A_RECEIVE, in, WAIT_C, WAIT_FLAG);                 	
-			            break;
-		            case WAIT_C:
-						currentState = verifyByte(C_UA, in, WAIT_BCC, WAIT_FLAG);                   
-						break;
-		            case WAIT_BCC: 
-			           currentState = verifyByte(A_RECEIVE^C_UA, in, BCC_OK, WAIT_FLAG);                 
-			           break;                   
-		            case BCC_OK:
-						currentState = verifyByte(FLAG, in, EXIT, WAIT_FLAG);                 
-		                break;
-		            default:
-		                perror("Something very strange happened\n");
-		                return -1;
-		                break;                        
-		        }
-			}
-			alarm(0);
+	      unsigned char in;
+	      if(!read(fd, &in, 1)){
+		      printf("Tou aqui\n");
+		      if(resend)
+			      goto send;
+		      else if (abort_send)
+			      return -1;
+		      continue;
+	      }
+
+	      printf("%x\n", in);
+	      printf("Current state is %d\n", currentState);
+
+	      switch(currentState){
+		      case WAIT_FLAG:
+			      currentState = verifyByte(FLAG, in, WAIT_A, WAIT_FLAG);                  
+			      break;
+		      case WAIT_A:
+			      currentState = verifyByte(A_RECEIVE, in, WAIT_C, WAIT_FLAG);                 	
+			      break;
+		      case WAIT_C:
+			      currentState = verifyByte(C_UA, in, WAIT_BCC, WAIT_FLAG);                   
+			      break;
+		      case WAIT_BCC: 
+			      currentState = verifyByte(A_RECEIVE^C_UA, in, BCC_OK, WAIT_FLAG);                 
+			      break;                   
+		      case BCC_OK:
+			      currentState = verifyByte(FLAG, in, EXIT, WAIT_FLAG);                 
+			      break;
+		      default:
+			      perror("Something very strange happened\n");
+			      return -1;
+			      break;                        
+	      }
+      }
+      alarm(0);
 	}
 
 	return fd;	
